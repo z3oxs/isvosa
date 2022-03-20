d045eb7c4e15e86d129a279dd3b56c7be3ca811f
z3oxs <z3ox1s@protonmail.com>
2022-03-20 02:41:31 -0300

    Perfomance improvement

--- send.go
+++ send.go
@@ -3,8 +3,31 @@ package isvosa
 import (
     "fmt"
     "strings"
+    "net/http"
+    "encoding/json"
+    "log"
+    "io"
+    "bytes"
 )
 
 func (b *Bot) Send(method interface{}) {
-    request(method, b.Token, strings.Split(fmt.Sprintf("%T", method), ".")[1])
+    endpoint := strings.Split(fmt.Sprintf("%T", method), ".")[1]
+    url := fmt.Sprintf("%s/bot%s/%s", baseURL, b.Token, endpoint)
+
+    data, _ := json.Marshal(method)
+    r, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+    if err != nil {
+        log.Fatal(err)
+
+    }
+
+    defer r.Body.Close()
+
+    if r.StatusCode != 200 {
+        var requestError Error
+        bytes, _ := io.ReadAll(r.Body)
+        json.Unmarshal(bytes, &requestError)
+
+        log.Printf("%s [%d]", requestError.Description, requestError.ErrorCode)
+    }
 }
